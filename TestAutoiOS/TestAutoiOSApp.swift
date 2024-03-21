b50ba551be0a04b5e6f929f60868e3bcba29acd7
userCTest <rui.reis@usercentrics.com>
2024-03-21 12:44:50 +0000

    Initial Commit

---
+++ TestAutoiOS/TestAutoiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAutoiOSApp.swift
+//  TestAutoiOS
+//
+//  Created by Rui Reis on 21/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestAutoiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
